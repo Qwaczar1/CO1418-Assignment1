443329327989541a466b53e1801652b55b43a808
Qwaczar <peterzhukovc@gmail.com>
2023-12-15 13:59:41 +0200

    updateing

---
+++ Script.js
@@ -0,0 +1,7 @@
+function createHeader(){
+    let header = document.createElement("header");
+    let logo = document.createElement("img")
+    logo.idset = "logo";
+    logo.srcset = "resources/coursework/assignment%201%20resources/images/UCLAN-white-orizontal.png"
+    header.append(logo)
+}
