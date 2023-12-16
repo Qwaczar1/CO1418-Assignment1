fa73cded2a1baafb65a873732fc80068e891418a
Qwaczar <peterzhukovc@gmail.com>
2023-12-16 20:42:25 +0200

    fully created the fully styalized and functional header + navigation menu

--- Script.js
+++
@@ -1,7 +0,0 @@
-function createHeader(){
-    let header = document.createElement("header");
-    let logo = document.createElement("img")
-    logo.idset = "logo";
-    logo.srcset = "resources/coursework/assignment%201%20resources/images/UCLAN-white-orizontal.png"
-    header.append(logo)
-}
